54b80a459780e0d29e280f56771823dc43046a6a
Oncu <oohancan@gmail.com>
2024-08-27 13:57:00 +0300

    Initial Commit

---
+++ Simple Calculator/Simple_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CalculatorApp.swift
+//  Simple Calculator
+//
+//  Created by Oncu Can on 27.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
